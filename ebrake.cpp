88b91d7a2b03866a8e4a98692f2a2acd5e9a2b05
Girsy Guzman <girsyd@gmail.com>
2026-01-17 02:28:11 +0100

    added distance monitoring function

--- ebrake.cpp
+++ ebrake.cpp
@@ -1,32 +1,44 @@
 #include <iostream>
 #include <stdio.h>
-#include <Winsock2.h>
-#include <Ws2tcpip.h>
-#include <Mswsock.h>
 #include "follower.cpp"
 #include "lead.cpp"
 #include <cmath>
 
-SOCKET trucksocket[4];
-void propagate_warning(SOCKET trucksock[]) {
-	char warnmessage[1024] = "EBRAKE";
-	for (int i = 0; i < sizeof(trucksock); i++) {
-		send(trucksock[i], warnmessage, sizeof(warnmessage), 0);
+
+bool distance_monitoring (float truckdistance, deque<float> frontdistance){
+	const float safedistance=20;//m
+	float lastdistance;
+	bool obstacle_detected = false;
+
+	if(frontdistance.front()<truckdistance){
+		//wait for new reading
+		if(frontdistance.size()>1){
+			if(frontdistance[1]>frontdistance[0]){
+				obstacle_detected=false;
+				//normal braking for distance control
+			} else if(frontdistance[1]<safedistance){
+					obstacle_detected=true;
+				}
+			frontdistance.pop_front();//x0=x1
+		}
 	}
-	return;
+	return obstacle_detected;
 }
 
-void emergencybraking(bool warning) {
-	propagate_warning(trucksocket);
-	float deceleration,actspeed;
-	actspeed=getspeed()*(1000/3600);//from km/h to m/s
+
+void emergencybraking(bool warning,deque<float> frontdistance,deque<float> currentspeed) {
+	//propagate_warning();
+	string warningcmd="EBRAKE";
+
+	float setspeed, setbrake, deceleration,actspeed;
+	actspeed=currentspeed.back()*(1000/3600);//from km/h to m/s
 
 	while(warning){
-		if (speed > 0) {
-			deceleration= pow(actspeed,2)/(getdistance()*2);// m/s^2
-			setbrake(deceleration);
+		if (currentspeed.back() > 0) {
+			deceleration= pow(actspeed,2)/(frontdistance.back()*2);// m/s^2
+			setbrake=deceleration;
 		} else {
-			setspeed(0); //park
+			setspeed=0; //park
 		}
 	}
 	return;
